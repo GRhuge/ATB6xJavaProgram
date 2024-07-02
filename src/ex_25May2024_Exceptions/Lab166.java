357a2148a8351d0308f157e921d4f6f5c4d4f381
Rahul <ghugerahul972@gmail.com>
2024-07-02 22:21:08 +0530

    new

--- src/ex_25May2024_Exceptions/Lab166.java
+++ src/ex_25May2024_Exceptions/Lab166.java
@@ -3,10 +3,16 @@ package ex_25May2024_Exceptions;
 public class Lab166 {
 
 	public static void main(String[] args) {
-		String s = args[0];
-		int x = Integer.parseInt(s);
+		String s = args[0]; //from Run Configuration we have passed 5 as an argument
+		int x = Integer.parseInt(s); // parseInt will convert String="5"; to int = 5;
 		int a = 10/x;
 		System.out.println(x);
 		System.out.println(a);
+		
+		// Test case for program
+		// No Input in run configuration argument - ArrayIndexOutOfBoundException
+		// 0 Input as an argument in run configuration - ArithmeticException
+		// "krsna" as an argument in run configuration - NumberFormatException
+		// Special characters like(!@#$%) as an argument in run configuration - NumberFormatException
 	}
 }
---
+++ src/ex_25May2024_Exceptions/Lab167_Checked_Exception.java
@@ -0,0 +1,20 @@
+package ex_25May2024_Exceptions;
+
+import java.io.FileInputStream;
+
+public class Lab167_Checked_Exception {
+
+	public static void main(String[] args) {
+		// Checked exceptions
+		try {
+		FileInputStream fis = new FileInputStream("e:/abc.txt");
+	    }
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		//Unchecked exceptions
+	//	int a = 20;
+	//	int c = a/0;
+	}
+
+}
