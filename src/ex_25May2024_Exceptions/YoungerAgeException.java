9b46326c43cb8566ee2b5a589a45946f667d5e92
Rahul <ghugerahul972@gmail.com>
2024-07-05 23:02:59 +0530

    hie

--- src/ex_25May2024_Exceptions/YoungerAgeException.java
+++ src/ex_25May2024_Exceptions/YoungerAgeException.java
@@ -7,17 +7,4 @@ public class YoungerAgeException extends RuntimeException {
 		super(msg);
 	}
 }
-class voting{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your age");
-		int age = sc.nextInt();
-		
-		if(age<18) {
-			throw new YoungerAgeException("You are not eligible for voting");
-		}
-		else {
-			System.out.println("You can vote");
-		}
-	}
-}
+
---
+++ src/ex_25May2024_Exceptions/voting.java
@@ -0,0 +1,24 @@
+package ex_25May2024_Exceptions;
+
+import java.util.Scanner;
+
+class voting{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your age");
+		int age = sc.nextInt();
+		
+		try {
+		if(age<18) {
+			throw new YoungerAgeException("You are not eligible for voting");
+		}
+		else {
+			System.out.println("You can vote successfully");
+		}
+	 }
+		catch(YoungerAgeException e) {
+			e.printStackTrace();
+		}
+		System.out.println("hola");
+	}
+}
