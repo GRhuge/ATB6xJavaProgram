c18ddf7cfe6ff919275afb8f829b4b1db1ea8b6d
Rahul <ghugerahul972@gmail.com>
2025-08-23 16:42:03 +0530

    today

--- src/ex_18Apr2024_Operators/Lab038.java
+++ src/ex_18Apr2024_Operators/Lab038.java
@@ -4,7 +4,7 @@ public class Lab038 {
 
 	public static void main(String[] args) {
 		int a =67;
-		boolean b = !(a > 20 && a < 78);
+		boolean b = !(67 > 20 && 67 < 78);
 		System.out.println(b);
 
 	}
